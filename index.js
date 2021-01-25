24bed5cb2b16bfe4c82f6ae9874ab28472b0dc13
Denis Ferrero <ferrero.denis@protonmail.com>
2021-01-25 21:56:45 +0100

    Added basic files

---
+++ index.js
@@ -0,0 +1,19 @@
+class migrations {
+  constructor (options) {
+
+  }
+
+  up (target_migration) {
+
+  }
+
+  down (target_migration) {
+
+  }
+
+  pending () {
+    
+  }
+}
+
+module.exports = migrations;
