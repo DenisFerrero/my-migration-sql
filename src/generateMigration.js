41e8a278c8f6a5643205399ea5f38bd936d12617
Denis Ferrero <ferrero.denis@protonmail.com>
2021-01-26 22:08:34 +0100

    Generate migrations file

---
+++ src/generateMigration.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+var migration_path = path.join(process.cwd(), 'migrations');
+// Flag cmd parser
+const args = require('yargs').argv;
+try {
+  if(!(args.name)) throw new Error('Undefined migration name');
+  // Parse migration path
+  if(args.path) migration_path = path.join(process.cwd(), args.path);
+  if(!(fs.existsSync(migration_path))) throw new Error(`${migration_path} folder not exists`); 
+  // Parsing date for filename (Solution by Daniel Cerecedo -> https://stackoverflow.com/questions/5914020/javascript-date-to-string)
+  var now = new Date();
+  var dateData = /^(\d{4})-(\d{2})-(\d{2})T.*$/.exec(now.toJSON());
+  var timeData = /^.*T(\d{2}):(\d{2}):(\d{2}).*$/.exec(now.toJSON());
+  var myFormat = `${dateData[1]}${dateData[2]}${dateData[3]}${timeData[1]}${timeData[2]}`;
+  // Create migration filename
+  var migration_filename = `${myFormat} - ${args.name}.js`;
+  // Create, write and save the new file
+  fs.writeFile(path.join(migration_path, migration_filename), `module.exports = {
+  "up": "",
+  "down": ""
+}`, { flag: 'w' }, function (err) {
+        try {
+          if(err) throw err;
+          console.log(`----- ${migration_filename} file created successfully! -----`);
+        } catch (err) { console.error(err); }
+      })
+} catch (err) { console.error(err) }
