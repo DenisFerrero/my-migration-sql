fd89b8f0ad7094906b462f1fead6f1bb68fb439f
Denis Ferrero <ferrero.denis@protonmail.com>
2021-01-27 18:05:42 +0100

    Loggin

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ class migrations {
   async pending () {
     let migration_files = fs.readdirSync(migration_path);
     let already_migrated = await saveOptions.getAllMigrated();
+    console.log(`Pending`, migration_files, already_migrated);
     // Remove already migrated
     return migration_files.filter(file => { !(already_migrated.find(mig => mig.name == file)) });
   }
